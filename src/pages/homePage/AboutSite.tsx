0618662e2b32ad93e4ead2f4f8828d7b6af9b8bc
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-28 20:23:03 +0530

    updated about saitama. . .

--- src/pages/homePage/AboutSite.tsx
+++ src/pages/homePage/AboutSite.tsx
@@ -1,14 +1,16 @@
-import { Flex, Spinner, Text, Image } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Spinner, Text, Image, Icon } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { MdCloseFullscreen } from 'react-icons/md';
 
 type AboutSiteProps = {
 
 };
 
 const AboutSite: React.FC<AboutSiteProps> = () => {
-
+    const [display, setDisplay] = useState(true);
     return (
         <Flex
+            display={display ? 'flex' : 'none'}
             width={'80%'}
             // height={'100px'}
             flexDirection={'column'}
@@ -23,20 +25,40 @@ const AboutSite: React.FC<AboutSiteProps> = () => {
             }}
         >
             <Flex
-                height={'40px'}
-                width={'100%'}
-                // border={'1px solid white'}
+                direction={'row'}
                 justify={'center'}
                 align={'center'}
+                width={'100%'}
             >
-                <Text
-                    color={'white'}
-                    fontSize={30}
-                    fontWeight={50}
+                <Flex
+                    height={'40px'}
+                    width={'80%'}
+                    // border={'1px solid white'}
+                    justify={'center'}
+                    align={'center'}
                 >
-                    WHAT IS SAITAMA??
-                </Text>
+                    <Text
+                        color={'white'}
+                        fontSize={30}
+                        fontWeight={50}
+                    >
+                        WHAT IS SAITAMA??
+                    </Text>
+                </Flex>
+                <Flex
+                    justifySelf={'flex-end'}
+                >
+                    <Icon
+                        as={MdCloseFullscreen}
+                        color={'white'}
+                        _hover={{
+                            fontSize: 30
+                        }}
+                        onClick={() => { setDisplay(false) }}
+                    />
+                </Flex>
             </Flex>
+
             <Flex
                 width={'95%'}
                 height={0.25}
@@ -60,17 +82,17 @@ const AboutSite: React.FC<AboutSiteProps> = () => {
                     <Text
                         color={'white'}
                     >
-                        It is a long established fact that a reader will be distracted by the readable content of
-                        a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
-                        normal distribution of letters, as opposed to using 'Content here, content here', making it look like
-                        readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default
-                        model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions
-                        have evolved over the years,
-                        sometimes by accident, sometimes on purpose (injected humour and the like).
+                        Saitama is a simple content sharing website, users can create boards and tag them by
+                        intrests(indexes) these intrests allows other users with similar intrests to join and contribute to the
+                        same communities. Any user can create a board, but before creating one you are advised to check below in
+                        BOARDS BY INTRESTS section to check if one already exists. After creating a board, any member of a board can list/ tag
+                        the board with the intrest tag from the same section. After listing your board in an intrest, the same tag appears on
+                        the board homepage and the board in also listed in the corresponding intrest on the HomePage. Users can also add new intrests
+                        whenever the existing ones do not suffice. :)
                     </Text>
                 </Flex>
             </Flex>
-        </Flex>
+        </Flex >
     )
 }
 export default AboutSite;
